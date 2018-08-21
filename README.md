9067f41461eea41bf8096f71baa54988dc129fe6
Jisk Attema <j.attema@esciencecenter.nl>
2018-08-21 09:37:11 +0200

    Update README

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
 # RAVI: Resources Assignment and VIsualization #
 * version 0.2
 
+This is a tool to help planning the assignment of people to projects.
+There are many tools like it, but this is our tool ;)
+
+Our specific version of the planning problem is:
+ * on the order of 50 projects and 50 engineers
+ * projects run typically between 6 months and 4 years
+ * accounting is based on hours, projects require between 1 and 4 FTE of work
+ * we plan with monthly precision
+ * 1 or 2 engineers plus a coordinator work on a project at the same time
+ * an engineer works on 1 to 5 projects
+ * an engineer can work parttime
+ * cross comparison with actually worked hours, as registered in a separate system is required
+
 # Requires #
 * Flask
 * SQLAlchemy
@@ -12,7 +25,14 @@ Setup a **virtualenv** containing Flask and SQLAlchemy:
 ```bash
 virtualenv env --system-site-packages
 . env/bin/activate
-pip install Flask SQLAlchemy
+pip -r requirements.txt
+```
+
+Setup a sqlite3 database with the necessary tables:
+```bash
+sqlite3 database.db
+sqlite> .read SCHEMA.sql
+sqlite> .quit
 ```
 
 # Running
@@ -22,19 +42,14 @@ pip install Flask SQLAlchemy
  flask run
 ```
 
-# Exporting hours from Exact
-* Go to "Projecten" => "Tijd- en kosteninvoer" => "Ingevoerde tijd en kosten"
-* Select tab "Exporteerbaar"
-* Make sure the following columns are included (add if necessary by the option "aanpassen" in the top-right menu):
-    * Projectcode
-    * Medewerker ID
-    * Datum
-    * Aantal
-    * Uur- of kostensoort (Code)
-* Choose "Exporteren: Excel" in the top-right menu
-* In Excel: Remove the top 13 or so lines to keep only one header line and data
-* Save as CSV, using "," as field delimiter and no text delimiter
+# Documentation
 
+See *README.tables* for a description of the main database tables.
+See *README.exact* on how to import written hours into the database.
+See *README.triggers* on a description of the audit log.
+
+User interface documentation: TODO
 
 # Contact #
 * l.ridder@esciencecenter.nl
+* j.attema@esciencecenter.nl
---
+++ SCHEMA.sql
@@ -0,0 +1,82 @@
+CREATE TABLE engineers (
+        eid VARCHAR NOT NULL,
+        exact_id VARCHAR,
+        fte FLOAT,
+        start INTEGER,
+        "end" INTEGER,
+        coordinator VARCHAR, comments unicode, active boolean default 1,
+        PRIMARY KEY (eid),
+        FOREIGN KEY(coordinator) REFERENCES engineers (eid)
+);
+CREATE TABLE projects (
+        pid VARCHAR NOT NULL,
+        exact_code VARCHAR,
+        fte FLOAT,
+        start INTEGER,
+        "end" INTEGER,
+        coordinator VARCHAR, comments unicode, active boolean default 1,
+        PRIMARY KEY (pid),
+        FOREIGN KEY(coordinator) REFERENCES engineers (eid)
+);
+CREATE TABLE assignments (
+        aid INTEGER NOT NULL,
+        fte FLOAT,
+        eid VARCHAR,
+        pid VARCHAR,
+        start INTEGER,
+        "end" INTEGER,
+        PRIMARY KEY (aid),
+        FOREIGN KEY(eid) REFERENCES engineers (eid),
+        FOREIGN KEY(pid) REFERENCES projects (pid)
+);
+CREATE TABLE hours(
+"Day" INTEGER,
+"Month" INTEGER,
+"Year" INTEGER,
+"Project" TEXT,
+"Projectcode" TEXT,
+"Medewerker" TEXT,
+"Medewerker ID" INTEGER,
+"Aantal" INTEGER
+);
+CREATE TABLE AUDIT(
+date VARCHAR,
+comment VARCHAR,
+aid INTEGER,
+oldfte FLOAT,
+oldeid VARCHAR,
+oldpid VARCHAR,
+oldstart INTEGER,
+oldend INTEGER,
+
+newfte FLOAT,
+neweid VARCHAR,
+newpid VARCHAR,
+newstart INTEGER,
+newend INTEGER,
+FOREIGN KEY(aid) REFERENCES assignments(aid),
+FOREIGN KEY(oldpid) REFERENCES projects(pid),
+FOREIGN KEY(newpid) REFERENCES projects(pid),
+                                                               FOREIGN KEY(oldeid) REFERENCES engineers(eid),
+FOREIGN KEY(neweid) REFERENCES engineers(eid)
+);
+CREATE TRIGGER assignmentnew AFTER INSERT ON assignments FOR EACH ROW
+BEGIN
+INSERT INTO AUDIT
+(date, comment, aid, newfte, neweid, newpid, newstart, newend)
+VALUES
+(date('now'), 'INSERT', new.aid, new.fte, new.eid, new.pid, new.start, new.end);
+END;
+CREATE TRIGGER assignmentdelete BEFORE DELETE ON assignments
+BEGIN
+INSERT INTO AUDIT(date, comment, aid, oldfte, oldeid, oldpid, oldstart, oldend)
+VALUES
+(date('now'), 'DELETE', old.aid, old.fte, old.eid, old.pid, old.start, old.end);
+END;
+CREATE TRIGGER assignmentupdate AFTER UPDATE ON assignments WHEN
+(old.fte <> new.fte OR old.eid <> new.eid OR old.pid <> new.pid OR old.start <> new.start OR old.end <> new.end)
+BEGIN
+INSERT INTO AUDIT(date, comment, aid, oldfte, oldeid, oldpid, oldstart, oldend, newfte, neweid, newpid, newstart, newend)
+VALUES
+(date('now'), 'UPDATE', old.aid, old.fte, old.eid, old.pid, old.start, old.end, new.fte, new.eid, new.pid, new.start, new.end);
+END;
